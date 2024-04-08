e0ac6ed19ab5b1de6155179569a04a5d11575dd6
Anish Aritakula <anisharitakula@gmail.com>
2024-04-08 10:03:47 +1000

    Added 01_Experiment.py file

---
+++ aml_service/01_Experiment.py
@@ -0,0 +1,20 @@
+import os
+from azureml.core import Experiment
+from azureml.core import Workspace
+from azureml.core.authentication import AzureCliAuthentication
+#cli_auth = AzureCliAuthentication()
+
+def getExperiment():
+    # Specify the path to your config.json file
+    config_path = 'AML_config/config.json'
+
+    ws = Workspace.from_config(path=config_path)
+    #script_folder = "."
+    experiment_name = "vision-model-devops"
+    exp = Experiment(workspace=ws, name=experiment_name)
+    print(exp.name, exp.workspace.name, sep="\n")
+    return exp
+
+
+if __name__ == "__main__":
+    exp = getExperiment()
